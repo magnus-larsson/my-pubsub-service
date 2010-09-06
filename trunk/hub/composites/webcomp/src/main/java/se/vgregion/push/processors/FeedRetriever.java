7a7a4e0fc6696b9e0e3335589610a51763225d4e
niklas@protocol7.com <niklas@protocol7.com@5de1423c-0f16-3ddf-91ed-b13b057e3ba5>
2010-09-06 14:40:13 +0000

    Moving to VGR RA structure

--- trunk/hub/composites/webcomp/src/main/java/se/vgregion/push/processors/FeedRetriever.java
+++ trunk/hub/composites/webcomp/src/main/java/se/vgregion/push/processors/FeedRetriever.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2010 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ *
+ */
+
 package se.vgregion.push.processors;
 
 import java.io.File;
@@ -38,6 +57,11 @@ public class FeedRetriever {
     }
 
     public void start() {
+        if(!feedDirectory.exists()) {
+            LOG.info("Creating feed directory at \"{}\"", feedDirectory.getAbsolutePath());
+            feedDirectory.mkdirs();
+        }
+        
         LOG.info("Starting FeedRetriever");
         executor.execute(new Runnable() {
             @Override
--- trunk/readme.txt
+++
@@ -1,6 +0,0 @@
-
-Run test instance:
-mvn jetty:run
-
-Test publishing:
-curl http://localhost:8080/ -v -d hub.mode=publish -d hub.url=http://localhost:8000/0e1383718a2889c12af18febb1a2e3de
