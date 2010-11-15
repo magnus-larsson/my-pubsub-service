0058e8f4a07a2071b5169e469ba1c254bc925213
niklas@protocol7.com <niklas@protocol7.com@5de1423c-0f16-3ddf-91ed-b13b057e3ba5>
2010-11-15 21:04:13 +0000

    Adding additional integration tests

--- trunk/hub/composites/webcomp/src/test/java/se/vgregion/push/inttest/IntegrationTest.java
+++ trunk/hub/composites/webcomp/src/test/java/se/vgregion/push/inttest/IntegrationTest.java
@@ -1,14 +1,11 @@
 package se.vgregion.push.inttest;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import se.vgregion.push.UnitTestConstants;
-import se.vgregion.push.types.FeedSerializer;
 import se.vgregion.push.types.ContentType;
 import se.vgregion.push.types.Feed;
 import se.vgregion.push.types.Entry.EntryBuilder;
@@ -17,22 +14,89 @@ import se.vgregion.push.types.Feed.FeedBuilder;
 public class IntegrationTest extends IntegrationTestTemplate {
 
     @Test
-    public void test() throws URISyntaxException, IOException, InterruptedException {
+    public void simpleAtomPublication() throws Exception {
         Feed feed = new FeedBuilder(UnitTestConstants.TOPIC, ContentType.ATOM).id("f1").updated(
                 UnitTestConstants.UPDATED1).entry(
                 new EntryBuilder().id("e1").updated(UnitTestConstants.UPDATED1).build()).entry(
                 new EntryBuilder().id("e2").updated(UnitTestConstants.UPDATED1).build()).build();
 
-        System.out.println(FeedSerializer.printFeed(feed).toXML());
-        
         publisher.publish(hubUrl, feed);
 
         Feed publishedFeed = publishedFeeds.poll(5000, TimeUnit.MILLISECONDS);
 
+        Assert.assertNotNull(publishedFeed);
         Assert.assertEquals("f1", publishedFeed.getFeedId());
         Assert.assertEquals(2, publishedFeed.getEntries().size());
-        
-        Assert.assertNotNull(publishedFeed);
-
     }
+
+    @Test
+    public void simpleRssPublication() throws Exception {
+        Feed feed = new FeedBuilder(UnitTestConstants.TOPIC, ContentType.RSS).id("f1").updated(
+                UnitTestConstants.UPDATED1).entry(
+                new EntryBuilder().id("e1").updated(UnitTestConstants.UPDATED1).build()).entry(
+                new EntryBuilder().id("e2").updated(UnitTestConstants.UPDATED1).build()).build();
+
+        publisher.publish(hubUrl, feed);
+
+        Feed publishedFeed = publishedFeeds.poll(5000, TimeUnit.MILLISECONDS);
+
+        Assert.assertNotNull(publishedFeed);
+        Assert.assertEquals("f1", publishedFeed.getFeedId());
+        Assert.assertEquals(2, publishedFeed.getEntries().size());
+    }
+
+    
+    @Test
+    public void doublePublication() throws Exception {
+        Feed feed = new FeedBuilder(UnitTestConstants.TOPIC, ContentType.ATOM).id("f1").updated(
+                UnitTestConstants.UPDATED2).entry(
+                new EntryBuilder().id("e1").updated(UnitTestConstants.UPDATED2).build()).entry(
+                new EntryBuilder().id("e2").updated(UnitTestConstants.UPDATED2).build()).build();
+        Feed feed2 = new FeedBuilder(UnitTestConstants.TOPIC, ContentType.ATOM)
+            .id("f1").updated(UnitTestConstants.FUTURE)
+            .entry(new EntryBuilder().id("e1").updated(UnitTestConstants.FUTURE).build())
+            .entry(new EntryBuilder().id("e2").updated(UnitTestConstants.UPDATED2).build())
+            .build();
+
+        publisher.publish(hubUrl, feed);
+
+        Feed publishedFeed = publishedFeeds.poll(5000, TimeUnit.MILLISECONDS);
+
+        Assert.assertNotNull(publishedFeed);
+        Assert.assertEquals("f1", publishedFeed.getFeedId());
+        Assert.assertEquals(2, publishedFeed.getEntries().size());
+
+        publisher.publish(hubUrl, feed2);
+
+        publishedFeed = publishedFeeds.poll(5000, TimeUnit.MILLISECONDS);
+
+        Assert.assertNotNull(publishedFeed);
+        Assert.assertEquals("f1", publishedFeed.getFeedId());
+        Assert.assertEquals(1, publishedFeed.getEntries().size());
+        Assert.assertEquals("e1", publishedFeed.getEntries().get(0).getEntryId());
+    }
+
+    @Test
+    public void doublePublicationWithoutUpdate() throws Exception {
+        Feed feed = new FeedBuilder(UnitTestConstants.TOPIC, ContentType.ATOM).id("f1").updated(
+                UnitTestConstants.UPDATED2).entry(
+                new EntryBuilder().id("e1").updated(UnitTestConstants.UPDATED2).build()).entry(
+                new EntryBuilder().id("e2").updated(UnitTestConstants.UPDATED2).build()).build();
+
+        publisher.publish(hubUrl, feed);
+
+        Feed publishedFeed = publishedFeeds.poll(5000, TimeUnit.MILLISECONDS);
+
+        Assert.assertNotNull(publishedFeed);
+        Assert.assertEquals("f1", publishedFeed.getFeedId());
+        Assert.assertEquals(2, publishedFeed.getEntries().size());
+
+        publisher.publish(hubUrl, feed);
+
+        publishedFeed = publishedFeeds.poll(5000, TimeUnit.MILLISECONDS);
+
+        // nothing should be pushed to the subscriber
+        Assert.assertNull(publishedFeed);
+    }
+
 }
