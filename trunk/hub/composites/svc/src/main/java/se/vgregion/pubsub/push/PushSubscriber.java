1d02099e6a358782bbb5f807e1a0903de98cf97a
niklas@protocol7.com <niklas@protocol7.com@5de1423c-0f16-3ddf-91ed-b13b057e3ba5>
2010-11-20 17:23:20 +0000

    Renaming modules

--- trunk/hub/composites/svc/src/main/java/se/vgregion/pubsub/push/PushSubscriber.java
+++
@@ -1,9 +0,0 @@
-package se.vgregion.pubsub.push;
-
-import se.vgregion.dao.domain.patterns.entity.Entity;
-import se.vgregion.pubsub.Subscriber;
-
-public interface PushSubscriber extends Subscriber, Entity<PushSubscriber, Long>  {
-
-
-}
--- trunk/hub/composites/types/src/main/java/se/vgregion/pubsub/Namespaces.java
+++
@@ -1,7 +0,0 @@
-package se.vgregion.pubsub;
-
-public class Namespaces {
-
-    public static final String NS_ATOM = "http://www.w3.org/2005/Atom";
-
-}
