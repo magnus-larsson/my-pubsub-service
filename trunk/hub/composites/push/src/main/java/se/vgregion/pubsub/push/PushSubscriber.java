1378e4ad898c0ac8b82d6c2bbdd97b9f0d3db912
niklas@protocol7.com <niklas@protocol7.com@5de1423c-0f16-3ddf-91ed-b13b057e3ba5>
2010-12-08 14:06:45 +0000

    Adding basic admin GUI

--- trunk/hub/composites/push/src/main/java/se/vgregion/pubsub/push/PushSubscriber.java
+++ trunk/hub/composites/push/src/main/java/se/vgregion/pubsub/push/PushSubscriber.java
@@ -11,6 +11,7 @@ public interface PushSubscriber extends Subscriber, Entity<Pair<URI, URI>>  {
     // 24 hours
     public static final int DEFAULT_LEASE_SECONDS = 60*60*24;
 
+    URI getCallback();
     
     void verify(SubscriptionMode mode) throws IOException, FailedSubscriberVerificationException;
 
