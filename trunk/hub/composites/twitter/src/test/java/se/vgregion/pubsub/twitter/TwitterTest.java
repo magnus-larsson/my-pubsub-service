c3b72b2c193f21f354ca87e20749dfcaf5f360da
niklas@protocol7.com <niklas@protocol7.com@5de1423c-0f16-3ddf-91ed-b13b057e3ba5>
2010-12-21 09:40:25 +0000

    Adding support for publishing from Twitter Stream API

--- trunk/hub/composites/twitter/src/test/java/se/vgregion/pubsub/twitter/TwitterTest.java
+++ trunk/hub/composites/twitter/src/test/java/se/vgregion/pubsub/twitter/TwitterTest.java
@@ -1,26 +1,20 @@
 package se.vgregion.pubsub.twitter;
 
-import java.io.IOException;
 import java.net.URI;
 
-import org.joda.time.DateTime;
-import org.junit.Test;
 import org.mockito.Mockito;
 
 import se.vgregion.pubsub.Feed;
 import se.vgregion.pubsub.PubSubEngine;
 import se.vgregion.pubsub.PubSubEventListener;
-import se.vgregion.pubsub.PublicationFailedException;
 import se.vgregion.pubsub.Subscriber;
 import se.vgregion.pubsub.Topic;
 import se.vgregion.pubsub.twitter.impl.TwitterPublisher;
-import se.vgregion.pubsub.twitter.impl.TwitterStreamConsumer;
 
 
 public class TwitterTest {
 
-    @Test
-    public void test() throws IOException, Exception {
+    public static void main(String[] args) throws InterruptedException {
         PubSubEngine engine = new PubSubEngine() {
             @Override
             public void publish(URI url, Feed feed) {
@@ -53,7 +47,7 @@ public class TwitterTest {
             }
         };
         
-        TwitterPublisher consumer = new TwitterPublisher(engine, "protocol7", "PUkcqKHN8J2OvU");
+        TwitterPublisher consumer = new TwitterPublisher(engine, "", "");
         Subscriber subscriber = Mockito.mock(Subscriber.class);
         Mockito.when(subscriber.getTopic()).thenReturn(URI.create(TwitterPublisher.FILTER_URL + "#" + "foo"));
         consumer.onSubscribe(subscriber);
